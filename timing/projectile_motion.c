2267857602d9a6057338a13b89fae8cdec19fbaf
Christopher Skane <sokrbball@gmail.com>
2021-12-19 18:33:40 -0500

    Updated to double-precision.

--- timing/projectile_motion.c
+++ timing/projectile_motion.c
@@ -16,18 +16,18 @@ int main(int argc,char **argv){
 
 	struct timeval tval_start,tval_current,tval_diff;// Will be used for timing
 
-	float START_Y_POS;// Height at which object is launched from
-	float INIT_VELOCITY;// Velocity in generic measurement (distance unit/second)
-	float INIT_ANGLE;// Launch angle in degrees
+	double START_Y_POS;// Height at which object is launched from
+	double INIT_VELOCITY;// Velocity in generic measurement (distance unit/second)
+	double INIT_ANGLE;// Launch angle in degrees
 
 	for(int i=0;i<argc;i++){// See if 'i' or '-i' is given to allow manual entry
 		if(argv[i][0] == 'i' || argv[i][1] == 'i'){
 			printf("Enter starting height (meters): ");
-			scanf("%f",&START_Y_POS);
+			scanf("%lf",&START_Y_POS);
 			printf("Enter Initial Velocity (m/s): ");
-			scanf("%f",&INIT_VELOCITY);
+			scanf("%lf",&INIT_VELOCITY);
 			printf("Enter Firing angle: ");
-			scanf("%f",&INIT_ANGLE);
+			scanf("%lf",&INIT_ANGLE);
 			break;
 		}else{
 			START_Y_POS = 10;// Height at which object is launched from
@@ -35,18 +35,18 @@ int main(int argc,char **argv){
 			INIT_ANGLE = 20;// Launch angle in degrees
 		}
 	}
-	float x_pos=0, y_pos = START_Y_POS;// Will be current stats of projectile in metres
-	float X_VELOCITY = INIT_VELOCITY * cos(INIT_ANGLE * M_PI/180.0);// Get horizontal speed
-	float Y_VELOCITY = INIT_VELOCITY * sin(INIT_ANGLE * M_PI/180.0);// Get vertical speed
-	float gravity = -9.8/pow((y_pos+6400)/6400,2);// Get gravity at different altitudes above surface
+	double x_pos=0, y_pos = START_Y_POS;// Will be current stats of projectile in metres
+	double X_VELOCITY = INIT_VELOCITY * cos(INIT_ANGLE * M_PI/180.0);// Get horizontal speed
+	double Y_VELOCITY = INIT_VELOCITY * sin(INIT_ANGLE * M_PI/180.0);// Get vertical speed
+	double gravity = -9.8/pow((y_pos+6400)/6400,2);// Get gravity at different altitudes above surface
 	long surface = 0;// Will be how far down the surface of the earth is, changes based on distance
 
-	printf("INITIAL HEIGHT: %.03fm\n",START_Y_POS);
-	printf("INITIAL SPEED: %.03fm/s\n",INIT_VELOCITY);
-	printf("ANGLE: %.03f degrees\n",INIT_ANGLE);
+	printf("INITIAL HEIGHT: %.03lfm\n",START_Y_POS);
+	printf("INITIAL SPEED: %.03lfm/s\n",INIT_VELOCITY);
+	printf("ANGLE: %.03lf degrees\n",INIT_ANGLE);
 	printf("LAUNCH!\n");
 
-	float cur_time;
+	double cur_time;
 	gettimeofday(&tval_start,NULL);// Get launch/start time
 	while(y_pos >= surface){// While projectile is still in flight
 		gettimeofday(&tval_current,NULL);
@@ -64,14 +64,14 @@ int main(int argc,char **argv){
 		// vert.accel AKA Gravity
 		// Gravity is -9.8 m/s/s at sea level
 
-		printf("\rTime: %5.04fs\t  X_POS: %11.5fm\t Y_POS: %8.06fm",cur_time,x_pos,y_pos);
+		printf("\rTime: %5.04lfs\t  X_POS: %11.5lfm\t Y_POS: %8.06lfm",cur_time,x_pos,y_pos);
 		printf("\tSurface: %ld",surface);
 		fflush(stdout);
-if(y_pos<0) printf("\n\nGravity: %5.06f\t  X_POS: %11.5fm\t Y_POS: %8.06fm\n\n",gravity,x_pos,y_pos);
+if(y_pos<0) printf("\n\nGravity: %5.06lf\t  X_POS: %11.5lfm\t Y_POS: %8.06lfm\n\n",gravity,x_pos,y_pos);
 	}
 	gettimeofday(&tval_current,NULL);
 	timersub(&tval_current,&tval_start,&tval_diff);
 	cur_time = tval_diff.tv_sec + (tval_diff.tv_usec / 1e6);
 
-	printf("\nLANDED\nProjectile went %.04f distance units in %.06f seconds.\n",x_pos,cur_time);
+	printf("\nLANDED\nProjectile went %.04lf distance units in %.06lf seconds.\n",x_pos,cur_time);
 }
