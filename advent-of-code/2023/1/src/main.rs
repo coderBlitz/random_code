10f83de6bf28c7c0ff8c38891cd4a5bf5db2afce
Christopher Skane <sokrbball@gmail.com>
2023-12-01 17:00:37 -0500

    Completed day 1 part 2.

--- advent-of-code/2023/1/src/main.rs
+++ advent-of-code/2023/1/src/main.rs
@@ -6,6 +6,52 @@ use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
+struct DigitIter<'a>(usize, &'a str);
+impl Iterator for DigitIter<'_> {
+	type Item = usize;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		let mut it = self.1.bytes().skip(self.0);
+		let len = self.1.len();
+		for i in self.0 .. self.1.len() {
+			self.0 += 1; // Increment early for simplicity
+
+			let b = it.next().unwrap() as usize;
+			let short_slice = &self.1[i .. (i + 3).min(len)];
+			let mid_slice = &self.1[i .. (i + 4).min(len)];
+			let long_slice = &self.1[i .. (i + 5).min(len)];
+
+			// If digit, just return value
+			if (0x30..=0x39).contains(&b){
+				return Some((b - 0x30) as usize)
+			}
+
+			// Check short digit names
+			if short_slice.find("one").is_some() {
+				return Some(1)
+			} else if short_slice.find("two").is_some() {
+				return Some(2)
+			} else if short_slice.find("six").is_some() {
+				return Some(6)
+			} else if mid_slice.find("four").is_some() {
+				return Some(4)
+			} else if mid_slice.find("five").is_some() {
+				return Some(5)
+			} else if mid_slice.find("nine").is_some() {
+				return Some(9)
+			} else if long_slice.find("three").is_some() {
+				return Some(3)
+			} else if long_slice.find("seven").is_some() {
+				return Some(7)
+			} else if long_slice.find("eight").is_some() {
+				return Some(8)
+			}
+		}
+
+		None
+	}
+}
+
 fn main() {
 	/* Get reader for input file
 	*/
@@ -25,17 +71,33 @@ fn main() {
 	*/
 	let mut line = String::new();
 	let mut _lines = 0;
-	let mut sum = 0;
+	let mut sum: usize = 0;
 	while let Ok(length) = reader.read_line(&mut line) {
 		if length == 0 {
 			break;
 		}
 
-		// Iterate line
-		let mut it = line.chars().filter(|c| c.is_digit(10));
-		let front = it.next().unwrap().to_digit(10).unwrap();
+		/* PART 1 */
+		// Iterate chars/bytes and extract all digits.
+		/*
+		let mut it = line.bytes().filter_map(|b| match b {
+			0x30..=0x39 => Some((b - 0x30) as usize),
+			_ => None,
+		});
+		let front = it.next().unwrap();
 		let back = match it.last() {
-			Some(d) => d.to_digit(10).unwrap(),
+			Some(v) => v,
+			None => front,
+		};
+
+		sum += front * 10 + back;
+		*/
+
+		/* PART 2 */
+		let mut di = DigitIter(0, &line[..]);
+		let front = di.next().unwrap();
+		let back = match di.last() {
+			Some(v) => v,
 			None => front,
 		};
 
