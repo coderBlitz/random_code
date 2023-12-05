b1dfbc14a5b85d773d4343fff28690cda07a0a77
Christopher Skane <sokrbball@gmail.com>
2023-12-04 19:15:46 -0500

    Completed day 4 part 2.

--- advent-of-code/2023/4/src/main.rs
+++ advent-of-code/2023/4/src/main.rs
@@ -3,7 +3,7 @@ taking a file as a command-line argument, as well as looping through all lines.
 */
 
 use std::{
-	collections::BTreeSet,
+	collections::{btree_map::Entry, BTreeMap, BTreeSet},
 	env,
 	fs::File,
 	io::{BufReader, BufRead},
@@ -29,13 +29,16 @@ fn main() {
 	let mut line = String::new();
 	let mut _lines = 0;
 	let mut sum = 0;
+	let mut card_counts = BTreeMap::new();
 	while let Ok(length) = reader.read_line(&mut line) {
 		if length == 0 {
 			break;
 		}
 
 		/* PART 1 */
-		let (_, numbers) = line.split_once(':').unwrap();
+		let (card, numbers) = line.split_once(':').unwrap();
+		let (_, card) = card.split_once(' ').unwrap();
+		let card: usize = card.trim_start().parse().unwrap();
 		let (winners, numbers) = numbers.split_once('|').unwrap();
 		let winners: BTreeSet<usize> = winners.trim().split_whitespace()
 			.map(|n| n.parse().unwrap())
@@ -49,9 +52,32 @@ fn main() {
 			sum += 2_usize.pow((common - 1) as u32);
 		}
 
+		/* PART 2 */
+		// Get current card count (multiplier for remaining)
+		let cur_count = match card_counts.entry(card) {
+			Entry::Vacant(v) => {
+				v.insert(1);
+				1
+			},
+			Entry::Occupied(mut o) => {
+				*o.get_mut() += 1;
+				*o.get()
+			},
+		};
+
+		for i in (card+1) ..= (card + common) {
+			match card_counts.entry(i) {
+				Entry::Vacant(v) => _ = v.insert(cur_count),
+				Entry::Occupied(mut o) => *o.get_mut() += cur_count,
+			};
+		}
+
 		// Per-iteration things
 		_lines += 1;
 		line.clear();
 	}
 	println!("Sum = {sum}");
+
+	let card_total: usize = card_counts.iter().map(|(_,v)| v).sum();
+	println!("Cards total = {card_total}");
 }
