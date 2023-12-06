0b156adfc5e3acb65da614696feaab37b079d24a
Christopher Skane <sokrbball@gmail.com>
2023-12-05 23:24:27 -0500

    Completed day 5 part 2.

--- advent-of-code/2023/5/src/main.rs
+++ advent-of-code/2023/5/src/main.rs
@@ -3,12 +3,85 @@ taking a file as a command-line argument, as well as looping through all lines.
 */
 
 use std::{
-	collections::{btree_map::Entry, BTreeMap},
 	env,
 	fs::File,
 	io::{BufReader, BufRead},
 	ops::Range,
 };
+
+fn overlap(a: &Range<usize>, b: &Range<usize>) -> bool {
+	a.start <= b.end && b.start <= a.end
+}
+fn intersect_range(a: &Range<usize>, b: &Range<usize>) -> Option<Range<usize>> {
+	if !overlap(a, b) {
+		return None
+	}
+
+	let start = a.start.max(b.start);
+	let end = a.end.min(b.end);
+	match start <= end {
+		true => Some(Range { start, end }),
+		false => None,
+	}
+}
+fn union_range(a: &Range<usize>, b: &Range<usize>) -> Option<Range<usize>> {
+	if !overlap(a, b) {
+		return None
+	}
+
+	let start = a.start.min(b.start);
+	let end = a.end.max(b.end);
+	match start <= end {
+		true => Some(Range { start, end }),
+		false => None,
+	}
+}
+fn symdif_range(a: &Range<usize>, b: &Range<usize>) -> Option<(Range<usize>, Option<Range<usize>>)> {
+	let intr = intersect_range(a,b)?;
+	let onion = union_range(a,b).unwrap(); // Intersect succeeded, so union will succeed
+
+	// Check if 1 or 2 ranges result
+	if intr == onion {
+		None
+	} else if onion.start == intr.start {
+		// 1 range
+		Some((Range { start: intr.end, end: onion.end }, None))
+	} else if onion.end == intr.end {
+		// 1 range
+		Some((Range { start: onion.start, end: intr.start }, None))
+	} else {
+		// 2 ranges
+		Some((Range { start: onion.start, end: intr.start }, Some(Range { start: intr.end, end: onion.end })))
+	}
+}
+
+// Count elements covered by all range elements (naive, ranges must not overlap).
+fn range_count(a: &[Range<usize>]) -> usize {
+	a.iter().map(|r| r.end - r.start).sum()
+}
+
+fn combine_ranges(v: &mut Vec<Range<usize>>) {
+	v.sort_by_key(|r| r.start);
+	// Combine overlapping ranges in-place.
+	if v.len() > 0 {
+		let mut k = 0;
+		let mut cur_rng = v[0].clone();
+		for i in 1..v.len() {
+			// Set scratch range to union of scratch and current, or current if not overlapping.
+			cur_rng = match union_range(&cur_rng, &v[i]) {
+				Some(r) => r,
+				None => {
+					v[k] = cur_rng;
+					k += 1;
+					v[i].clone()
+				},
+			}
+		}
+		v[k] = cur_rng; // Add remainder range
+		k += 1;
+		v.truncate(k);
+	}
+}
 
 fn main() {
 	/* Get reader for input file
@@ -32,17 +105,26 @@ fn main() {
 
 	// Get seeds from top of file
 	_ = reader.read_line(&mut buf);
-	let mut nums: Vec<usize> = buf.strip_prefix("seeds: ").unwrap()
+	let nums: Vec<usize> = buf.strip_prefix("seeds: ").unwrap()
 		.trim()
 		.split_whitespace()
 		.map(|n| n.parse().unwrap())
 		.collect();
-	nums.sort();
 
-	// Iterate rest of file with maps
+	let i1 = nums.iter().skip(1).step_by(2);
+	let mut ranges: Vec<Range<usize>> = nums.iter().step_by(2).zip(i1)
+		.map(|(&s, &l)| Range {
+			start: s,
+			end: s+l
+		}).collect();
+	ranges.sort_by_key(|r| r.start);
+	println!("Ranges {ranges:?}");
+	println!("Coverage {}", range_count(&ranges[..]));
+	let coverage = range_count(&ranges[..]);
+
 	buf.clear();
-	let mut new_map = BTreeMap::new();
-	new_map.extend(nums.iter().map(|v| (v,v)));
+	println!("Loop start");
+	let mut new_ranges = Vec::new();
 	while let Ok(length) = reader.read_line(&mut buf) {
 		let line = buf.trim();
 		if length == 0 {
@@ -52,18 +134,15 @@ fn main() {
 			buf.clear();
 			continue;
 		} else if line.contains(':') {
-			// Insert unmapped nums
-			for &n in nums.iter() {
-				if let Entry::Vacant(v) = new_map.entry(n) {
-					v.insert(n);
-				};
-			}
+			println!("----- RESET -----");
 
-			// Clear nums and insert mapped values
-			nums.clear();
-			nums.extend(new_map.iter().map(|(_,&v)| v));
-			new_map.clear();
-			println!("Nums now: {nums:?}");
+			// Combine working and scratch vector.
+			// Sort combined ranges (by start value).
+			// Combine overlapping ranges between.
+			// Clear scratch vector.
+			ranges.extend(new_ranges.drain(..)); // Combine
+			combine_ranges(&mut ranges);
+			assert_eq!(coverage, range_count(&ranges[..])); // If this panics, we lost/gained seeds.
 
 			_lines += 1;
 			buf.clear();
@@ -78,15 +157,29 @@ fn main() {
 		let len: usize = it.next().unwrap().parse().unwrap();
 
 		let src_range = Range { start: src_s, end: src_s + len };
-		println!("Src range: {src_range:?}");
+		let _dst_range = Range { start: dst_s, end: dst_s + len };
+		println!("Map {src_range:?} --> {_dst_range:?}");
 
-		// Map
-		for n in nums.iter_mut() {
-			if src_range.contains(n) {
-				if let Entry::Vacant(v) = new_map.entry(*n) {
-					v.insert(dst_s + (*n - src_s));
-				};
+		/*
+		*/
+		let mut i = 0;
+		while i < ranges.len() {
+			if let Some(r) = intersect_range(&ranges[i], &src_range) {
+				new_ranges.push(Range { start: dst_s + (r.start - src_s), end: dst_s + (r.end - src_s) }); // Map to new range
+
+				// If there's any range leftover, modify current and/or add split
+				if let Some((r1, or2)) = symdif_range(&ranges[i], &r) {
+					ranges[i] = r1;
+					if let Some(r2) = or2 {
+						ranges.push(r2);
+					}
+				} else {
+					ranges.remove(i);
+					continue; // Skip increment
+				}
 			}
+
+			i += 1;
 		}
 
 		// Per-iteration things
@@ -94,18 +187,10 @@ fn main() {
 		buf.clear();
 	}
 
-	// Insert unmapped nums
-	for &n in nums.iter() {
-		if let Entry::Vacant(v) = new_map.entry(n) {
-			v.insert(n);
-		};
-	}
+	ranges.extend(new_ranges.drain(..)); // Combine
+	combine_ranges(&mut ranges);
+	println!("Final: {ranges:?}");
+	assert_eq!(coverage, range_count(&ranges[..])); // If this panics, we lost/gained seeds.
 
-	// Clear nums and insert mapped values
-	nums.clear();
-	nums.extend(new_map.iter().map(|(_,&v)| v));
-	new_map.clear();
-
-	println!("Final nums: {nums:?}");
-	println!("Min is {}", nums.iter().min().unwrap());
+	println!("Min: {:?}", ranges[0].start);
 }
