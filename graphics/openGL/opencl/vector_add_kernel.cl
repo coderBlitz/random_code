c2a7d7aea7b1a0c6907002de32156154edd2d715
Christopher Skane <sokrbball@gmail.com>
2024-11-15 15:42:21 -0500

    Split out graphics.

--- graphics/openGL/opencl/vector_add_kernel.cl
+++
@@ -1,4 +0,0 @@
-__kernel void vector_add(__global float *A){
-	int id = get_global_id(0);
-	if(A[id]) A[id] = -A[id];
-}
--- graphics/openGL/tutorials/fshader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-uniform vec2 WindowSize;
-out vec3 color;
-
-void main(){
-	color = vec3(1, gl_FragCoord.x/WindowSize.x, 0);
-
-}
